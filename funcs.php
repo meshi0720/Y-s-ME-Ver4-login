c45944d41c15882ee6d84b5032a0e64b5d98a58c
meshi0720 <meshi0720@hotmail.co.jp>
2025-01-10 13:06:25 +0900

    Update funcs.php

--- funcs.php
+++ funcs.php
@@ -10,10 +10,10 @@ function h($str)
 function db_conn()
 {
     try {
-        $db_name = 'schoolchoice_db';    //データベース名
-        $db_id   = 'schoolchoice_db';      //アカウント名
-        $db_pw   = 'schoolchoice1221';      //パスワード：XAMPPはパスワード無しに修正してください。
-        $db_host = 'lmysql3104.db.sakura.ne.jp'; //DBホスト
+        $db_name = '';    //データベース名
+        $db_id   = '';      //アカウント名
+        $db_pw   = '';      //パスワード：XAMPPはパスワード無しに修正してください。
+        $db_host = ''; //DBホスト
         $pdo = new PDO('mysql:dbname=' . $db_name . ';charset=utf8;host=' . $db_host, $db_id, $db_pw);
         return $pdo;
     } catch (PDOException $e) {
